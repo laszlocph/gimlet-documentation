17b07be7d8c353c8a4442d583b12248ee0bbed67
Laszlo Fogas <laszlo@gimlet.io>
2023-01-09 13:31:52 +0100

    Edits

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -38,14 +38,14 @@ export function Hero() {
               </div>
               <div>
                 <h1 className="text-4xl font-bold tracking-tight dark:text-slate-100 sm:text-center sm:text-6xl">Need to deploy to Kubernetes?</h1>
-                <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300 sm:text-center">But not sure how to put things together? We got you covered.</p>
+                <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300 sm:text-center">But not sure how to put things together? We got you covered. Gimlet is a gitops based developer platform that gives you the best of open-source out of the box.</p>
                 <div className="mt-8 md:flex gap-x-4 sm:justify-center">
                   <a href="https://forms.reform.app/p5JfBA/early-access/nbZ6Go" className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700">
                     Sign up for our SaaS Early Access
                     <span className="text-indigo-200" aria-hidden="true">&rarr;</span>
                   </a>
                   <a href="/docs" className="mt-4 md:mt-0 inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 dark:text-gray-300 ring-1 ring-gray-900/10 dark:ring-gray-100/10 hover:ring-gray-900/20">
-                    Learn about the open-source version
+                    Self-Host Gimlet
                     <span className="text-gray-400" aria-hidden="true">&rarr;</span>
                   </a>
                 </div>
