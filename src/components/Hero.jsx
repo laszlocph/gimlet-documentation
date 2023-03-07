2991531d497757aa0b1fa8f7bf8156fda6dd4870
Laszlo Fogas <laszlo@gimlet.io>
2023-03-07 15:49:14 +0100

    Link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -48,7 +48,6 @@ export function Hero() {
                   <a
                     href="https://accounts.gimlet.io"
                     className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
-                    target="_blank"
                     onClick={() => Fathom.trackGoal('XPL4AWPN', 0)}
                     >
                     Sign up for our SaaS Early Access
