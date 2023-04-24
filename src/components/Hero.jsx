1614ae248a88aaabe3589f1d5b2d29ee4bfc63f5
Ádám Sisák <86593861+dzsak@users.noreply.github.com>
2023-04-24 10:35:37 +0200

    Button style fix (#27)
    * Button style fix
    
    * Style fix

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -27,7 +27,7 @@ export function Hero() {
 
     }, 1500);
   }
-  
+
   return (
     <div className="overflow-hidden dark:-mb-32 dark:-mt-[4.5rem] dark:pb-32 dark:pt-[4.5rem] dark:lg:-mt-[4.75rem] dark:lg:pt-[4.75rem]">
       <main>
@@ -44,19 +44,19 @@ export function Hero() {
               <div>
                 <h1 className="text-4xl font-bold tracking-tight dark:text-slate-100 sm:text-center sm:text-6xl">Need to deploy to Kubernetes?</h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300 sm:text-center">But not sure how to put things together? We got you covered. Gimlet is a gitops based developer platform that gives you the best of open-source out of the box. The platform you would build, given enough time.</p>
-                <div className="mt-8 md:flex gap-x-4 sm:justify-center">
+                <div className="mt-8 md:flex md:space-x-4 space-x-2 sm:justify-center">
                   <a
                     href="https://accounts.gimlet.io/signup/"
                     className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
                     onClick={() => Fathom.trackGoal('XPL4AWPN', 0)}
-                    >
+                  >
                     Sign up
                   </a>
                   <a
                     href="/docs"
                     className="mt-4 md:mt-0 inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 dark:text-gray-300 ring-1 ring-gray-900/10 dark:ring-gray-100/10 hover:ring-gray-900/20"
                     onClick={() => Fathom.trackGoal('VEWYCI7B', 0)}
-                    >
+                  >
                     Self-Host Gimlet
                   </a>
                 </div>
