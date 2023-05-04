2a0e1f082dac23757508b73b1d7d2788f561bbac
Laszlo Fogas <laszlo@gimlet.io>
2023-05-04 13:23:35 +0200

    CTA

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -32,7 +32,7 @@ export function Hero() {
     <div className="overflow-hidden dark:-mb-32 dark:-mt-[4.5rem] dark:pb-32 dark:pt-[4.5rem] dark:lg:-mt-[4.75rem] dark:lg:pt-[4.75rem]">
       <main>
         <div className="relative px-6 lg:px-8">
-          <div className="mx-auto max-w-3xl pt-20 pb-32 sm:pt-48 sm:pb-40">
+          <div className="mx-auto max-w-4xl pt-20 pb-32 sm:pt-48 sm:pb-40">
             <div>
               <div className="hidden sm:mb-8 sm:flex sm:justify-center">
                 <div className="relative overflow-hidden rounded-full py-1.5 px-4 text-sm leading-6 ring-1 ring-gray-900/10 hover:ring-gray-900/20 dark:ring-gray-100/10">
@@ -42,7 +42,7 @@ export function Hero() {
                 </div>
               </div>
               <div>
-                <h1 className="text-4xl font-bold tracking-tight dark:text-slate-100 sm:text-center sm:text-6xl">Kubernetes tooling packaged for SMBs, agencies and growing startups.</h1>
+                <h1 className="text-4xl font-bold tracking-tight dark:text-slate-100 sm:text-center sm:text-6xl">Kubernetes tooling packaged for small businesses, agencies and growing startups.</h1>
                 <p className="mt-6 text-lg leading-8 text-gray-600 dark:text-gray-300 sm:text-center">Gimlet is a gitops based developer platform that gives you the best of open-source out of the box. The platform you would build, given enough time.</p>
                 <div className="mt-8 md:flex md:space-x-4 space-x-2 sm:justify-center">
                   <a
@@ -50,14 +50,14 @@ export function Hero() {
                     className="inline-block rounded-lg bg-indigo-600 px-4 py-1.5 text-base font-semibold leading-7 text-white shadow-sm ring-1 ring-indigo-600 hover:bg-indigo-700 hover:ring-indigo-700"
                     onClick={() => Fathom.trackGoal('XPL4AWPN', 0)}
                   >
-                    Sign up
+                    Get started
                   </a>
                   <a
                     href="/docs"
                     className="mt-4 md:mt-0 inline-block rounded-lg px-4 py-1.5 text-base font-semibold leading-7 text-gray-900 dark:text-gray-300 ring-1 ring-gray-900/10 dark:ring-gray-100/10 hover:ring-gray-900/20"
                     onClick={() => Fathom.trackGoal('VEWYCI7B', 0)}
                   >
-                    Self-Host Gimlet
+                    Read more
                   </a>
                 </div>
               </div>
