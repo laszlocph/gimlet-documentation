36210a5940d0e966b9d0ee3f57f38da663385180
Laszlo Fogas <laszlo@gimlet.io>
2023-08-23 13:24:06 +0200

    Revert "Test"
    This reverts commit a06ce70b837addc0e98bf4f5ec087cb797345950.

--- src/pages/_app.jsx
+++ src/pages/_app.jsx
@@ -7,8 +7,6 @@ import { Layout } from '@/components/Layout'
 import 'focus-visible'
 import '@/styles/tailwind.css'
 import * as Fathom from "fathom-client";
-import Script from 'next/script'
-
 
 const navigation = [
   {
@@ -195,15 +193,6 @@ export default function App({ Component, pageProps }) {
           defer>
         </script> */}
       </Head>
-      <Script src="https://www.googletagmanager.com/gtag/js?id=G-Z0SWZFM3YZ" />
-      <Script id="google-analytics">
-        {`
-          window.dataLayer = window.dataLayer || [];
-          function gtag(){dataLayer.push(arguments);}
-          gtag('js', new Date());
-          gtag('config', 'G-Z0SWZFM3YZ');
-        `}
-      </Script>
       <Layout
         navigation={navigation}
         title={title}
