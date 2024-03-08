d258aa6e8a1d0ec5a01c877b9b68f280cbb61ed2
dzsak <sisakadam@gmail.com>
2024-03-08 09:41:20 +0100

    Card style fix

--- src/components/Languages.jsx
+++ src/components/Languages.jsx
@@ -36,18 +36,17 @@ export default function Languages() {
                 {items.map(i => {
                   return (
                     <div key={i.title}
-                      className="group overflow-hidden rounded-xl bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]"
+                      className="group flex flex-col justify-between overflow-hidden rounded-xl bg-white [box-shadow:0_0_0_1px_rgba(0,0,0,.03),0_2px_4px_rgba(0,0,0,.05),0_12px_24px_rgba(0,0,0,.05)] transform-gpu dark:bg-transparent dark:backdrop-blur-md dark:[border:1px_solid_rgba(255,255,255,.1)] dark:[box-shadow:0_-20px_80px_-20px_#ffffff1f_inset]"
                     >
-                      <div><img className="absolute -right-20 -top-20 opacity-60" /></div>
-                      <div className="pointer-events-none z-10 flex transform-gpu flex-col gap-1 p-6 transition-all duration-300 group-hover:-translate-y-10">
-                        <img src={i.source} className="h-14 w-14 origin-left transform-gpu text-neutral-700 transition-all duration-300 ease-in-out group-hover:scale-75"/>
+                      <div className="pointer-events-none z-10 flex flex-col gap-1 p-6">
+                        <img src={i.source} alt={i.title} className="h-14 w-14 origin-left transform-gpu text-neutral-700" />
                         <h3 className="text-xl font-semibold text-neutral-700 dark:text-neutral-300">{i.title}</h3>
                         <p className="max-w-lg text-neutral-400">{i.description}</p>
                       </div>
-                      <div className="pointer-events-none absolute bottom-0 flex w-full translate-y-10 transform-gpu flex-row items-center p-4 opacity-0 transition-all duration-300 group-hover:translate-y-0 group-hover:opacity-100">
+                      <div className="pointer-events-none bottom-0 flex w-full flex-row items-center p-4 mt-auto">
                         <a
                           href="#"
-                          className="inline-flex items-center justify-center font-medium text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-300 transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-xs pointer-events-auto"
+                          className="inline-flex items-center justify-center font-medium text-neutral-900 dark:text-neutral-100 dark:hover:text-neutral-300 transition-colors focus-visible:outline-none focus-visible:ring-1 focus-visible:ring-ring disabled:pointer-events-none disabled:opacity-50 hover:bg-accent hover:text-accent-foreground h-8 rounded-md px-3 text-xs pointer-events-auto underline"
                         >
                           {i.deploy}
                           <svg width="15" height="15" viewBox="0 0 15 15" fill="none" xmlns="http://www.w3.org/2000/svg" className="ml-2 h-4 w-4">
